84cb322c79364dc3e6abdc2c80791899e3480711
Kyujeong <kyuj1102@gmail.com>
2022-10-31 05:38:57 +0900

    splash screen & delay

--- instagram_ios_duckduck/AppDelegate.swift
+++ instagram_ios_duckduck/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //스플래시 화면 딜레이 시간
+        sleep(1)
         return true
     }
 
